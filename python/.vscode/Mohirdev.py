aa34a2b5bf323e727b918f8555e5c07ae2970b81
Muhammad Said <muhammad810s6443@gmail.com>
2026-01-10 16:40:46 +0500

    Refactor Mohirdev.py by commenting out OpenCV code for face and eye detection, preserving structure for future use.

--- python/.vscode/Mohirdev.py
+++ python/.vscode/Mohirdev.py
@@ -1,29 +1,34 @@
-import cv2
 
-cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-while True:
-    ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
-        roi_gray = gray[y:y+w, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
 
-    cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+# import cv2
 
-cap.release()
-cv2.destroyAllWindows()
+# cap = cv2.VideoCapture(0)
+# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# while True:
+#     ret, frame = cap.read()
+
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#     for (x, y, w, h) in faces:
+#         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+#         roi_gray = gray[y:y+w, x:x+w]
+#         roi_color = frame[y:y+h, x:x+w]
+#         eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+#         for (ex, ey, ew, eh) in eyes:
+#             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+
+#     cv2.imshow('frame', frame)
+
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
 
 # copyright Tim Ruscia aka techwithtim
 # code from https://github.com/techwithtim/OpenCV-Tutorials
@@ -31,7 +36,7 @@ cv2.destroyAllWindows()
 # #39-dars Tashqi kutibxona
 
 # from fuzzywuzzy import fuzz
-# from fuzzywuzzy import process  
+# from fuzzywuzzy import process
 # from uzwords1 import words
 # print(fuzz.ratio("salom","salam"))  #matnlar orasidagi o'xshashlik darajasini aniqlash)
 # print(fuzz.partial_ratio("salm","salom"))
